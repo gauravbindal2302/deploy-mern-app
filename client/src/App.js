26c879ba2c933556ee075069f0764f6bc64f49d2
Gaurav Bindal <gb2302bindal@gmail.com>
2024-02-08 22:35:04 +0530

    Updated multiple code files for production

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [list, setList] = useState([]);
+  const [addItemText, setAddItemText] = useState("");
+  const [editItemText, setEditItemText] = useState("");
+  const [editingItem, setEditingItem] = useState(null);
+
+  useEffect(() => {
+    fetchItems();
+  }, [list]);
+
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get(
+        "https://deploy-mern-app-server.vercel.app/get"
+      );
+      setList(response.data);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    }
+  };
+
+  const handleAdd = async () => {
+    if (addItemText) {
+      try {
+        await axios.post("https://deploy-mern-app-server.vercel.app/add", {
+          itemName: addItemText,
+        });
+        setAddItemText(""); // Clear add input field after adding
+        fetchItems(); // Fetch items after the state is updated
+      } catch (error) {
+        console.error("Error adding item:", error);
+      }
+    }
+  };
+
+  const handleUpdate = async (itemName) => {
+    try {
+      await axios.put(
+        `https://deploy-mern-app-server.vercel.app/update/${itemName}`,
+        {
+          updatedItemName: editItemText,
+        }
+      );
+      setEditingItem(null); // Stop editing
+      setEditItemText(""); // Clear edit input field
+      fetchItems(); // Fetch items after the state is updated
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleEdit = (itemName) => {
+    setEditingItem(itemName); // Start editing the item
+    const itemToEdit = list.find((item) => item.itemName === itemName);
+    setEditItemText(itemToEdit.itemName); // Set edit input text to the item's current text
+  };
+
+  const handleRemove = async (itemName) => {
+    try {
+      await axios.delete(
+        `https://deploy-mern-app-server.vercel.app/delete/${itemName}`
+      );
+      console.log("Item removed:", itemName);
+      fetchItems(); // Fetch items after the state is updated
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+  const handlePrint = () => {
+    window.print();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MERN App</h1>
+      <div className="operations">
+        <input
+          type="text"
+          placeholder="Enter text to add"
+          value={addItemText}
+          onChange={(e) => setAddItemText(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add</button>
+        <button onClick={handlePrint}>Print this page</button>
+      </div>
+      {list.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>S.No.</th>
+              <th>Items</th>
+              <th>Manage</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>
+                  {editingItem === item.itemName ? (
+                    <input
+                      type="text"
+                      value={editItemText}
+                      onChange={(e) => setEditItemText(e.target.value)}
+                    />
+                  ) : (
+                    item.itemName
+                  )}
+                </td>
+                <td>
+                  {editingItem === item.itemName ? (
+                    <button onClick={() => handleUpdate(item.itemName)}>
+                      Confirm
+                    </button>
+                  ) : (
+                    <button onClick={() => handleEdit(item.itemName)}>
+                      Edit
+                    </button>
+                  )}
+                </td>
+                <td>
+                  <button onClick={() => handleRemove(item.itemName)}>
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h1 className="no-items">No items to display.</h1>
+      )}
     </div>
   );
 }
-
-export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,85 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express();
+const server = express();
 
-app.use("/", (req, res) => {
-  res.send("Server is running!");
+server.use(cors());
+server.use(express.json());
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT;
+const DB = process.env.DATABASE;
+
+// Connect to MongoDB
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+// Create a schema and model for item
+const itemSchema = new mongoose.Schema({
+  itemName: String,
+});
+const Item = mongoose.model("items", itemSchema);
+
+// Route for adding item
+server.post("/add", async (req, res) => {
+  const { itemName } = req.body;
+  try {
+    const newItem = new Item({ itemName });
+    await newItem.save();
+    res.status(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+// Route for getting all items
+server.get("/get", async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).json(items);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+});
+
+// Route for updating an item
+server.put("/update/:itemName", async (req, res) => {
+  const { itemName } = req.params;
+  const { updatedItemName } = req.body;
+  try {
+    await Item.findOneAndUpdate({ itemName }, { itemName: updatedItemName });
+    res.status(200).send("Item updated successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error updating item");
+  }
+});
+
+// Route for deleting an item
+server.delete("/delete/:itemName", async (req, res) => {
+  const { itemName } = req.params;
+  try {
+    await Item.deleteOne({ itemName });
+    res.status(200).send("Item deleted successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error deleting item");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
